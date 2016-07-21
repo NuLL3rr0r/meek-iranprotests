edc089e5af06eebf6a5b1d84d278082746a22c48
David Fifield <david@bamsoftware.com>
2016-07-21 16:04:54 -0700

    Link to #18904.

--- meek-client-torbrowser/mac.go
+++ meek-client-torbrowser/mac.go
@@ -9,6 +9,7 @@ const (
 	// During startup of meek-client-torbrowser, the browser profile is
 	// created and maintained under firefoxProfilePath by making a
 	// recursive copy of everything under profileTemplatePath.
+	// https://bugs.torproject.org/18904
 	firefoxPath         = "../firefox"
 	firefoxProfilePath  = "../../../../TorBrowser-Data/Tor/PluggableTransports/profile.meek-http-helper"
 	profileTemplatePath = "../../Resources/TorBrowser/Tor/PluggableTransports/template-profile.meek-http-helper"
